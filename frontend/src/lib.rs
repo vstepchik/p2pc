ce65374b7a9300769c8e666c8b5f43b61641ffc3
Vitalik Stepchik <vitalik.stepchik@omnixlabs.com>
2020-04-12 01:01:23 +0300

    added console error panic hook

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,10 +1,11 @@
 mod app;
 
+use std::panic;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn run_app() -> Result<(), JsValue> {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
     yew::start_app::<app::App>();
-
     Ok(())
 }
